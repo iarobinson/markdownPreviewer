ac687fef921e7c11f6528314608161e54da5320b
Ian <ian (at) ianrobinson.net>
2018-02-22 19:46:26 -0500

    Add market via CDN link

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-console.log('JS Linked');
+console.log("script loaded");
 
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+});
+
+console.log(marked('I am using __markdown__.'));
